32b2dddf1e2a22a7437eb9b77951ed631236d69c
panargirakis <panargirakis@gmail.com>
2019-12-11 23:05:55 -0500

    trained a pretty good neural net

--- neural_net.py
+++ neural_net.py
@@ -1,24 +1,12 @@
-import pandas as pd
-from plotly.offline import iplot, plot
-import plotly.graph_objs as go
-import plotly.figure_factory as ff
-from scipy import stats
 import numpy as np
-import math
-from sklearn import datasets
-from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
-from sklearn.model_selection import KFold
-from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
-from itertools import product
-from sklearn.decomposition import PCA
+import pandas as pd
+from joblib import dump, load
 from sklearn.neural_network import MLPClassifier
 
 
 def balance_class_2(df):
     all_class_2 = df.loc[df["y"] == 2]
-    for i in range(100):
+    for i in range(80):
         row = all_class_2.iloc[np.random.randint(0, len(all_class_2))]
         df = df.append(row, ignore_index=True)
     df = df.sample(frac=1).reset_index(drop=True)  # shuffle the data
@@ -45,11 +33,13 @@ df = df.sample(frac=1).reset_index(drop=True)  # shuffle the data
 # parameters
 
 max_iter = 1000
-num_of_hidden_l = 2
-hl_size = 20
+num_of_hidden_l = 1
+hl_size = 10
 split = 0.8
 n_samples = len(df)
 split_index = int(n_samples * split)
+retrain_model = False
+filename = 'neural_network.joblib'
 
 # df = (df-df.min())/(df.max()-df.min())  # normalize
 
@@ -60,7 +50,7 @@ training_set = df.iloc[:split_index, :]
 # Balance class 2
 
 training_set = balance_class_2(training_set)
-print("Class 2 balanced. Added 100 samples. New sample size of class 2: ", len(training_set.loc[training_set["y"] == 2]))
+print("Class 2 balanced. Added 80 samples. New sample size of class 2: ", len(training_set.loc[training_set["y"] == 2]))
 
 train_X = training_set.iloc[:, :-1]
 # train_X = np.array(train_X, np.float32).reshape([-1, 1])
@@ -85,16 +75,23 @@ layers = []
 for i in range(num_of_hidden_l):
     layers.append(hl_size)
 
-nn = MLPClassifier(hidden_layer_sizes=layers, activation="tanh", verbose=False, max_iter=max_iter, solver="lbfgs")
-nn.fit(train_X, train_Y)
+if retrain_model:
+    nn = MLPClassifier(hidden_layer_sizes=layers, activation="tanh", verbose=False, max_iter=max_iter, solver="lbfgs")
+    nn.fit(train_X, train_Y)
+else:
+    nn = load(filename)
+dump(nn, filename)  # save the model
+
 print("Training accuracy of the nn: ", nn.score(train_X, train_Y))
 
 print("\nOverall testing accuracy of nn: ", nn.score(test_X, test_Y), "\n")
 
 # accuracy per class
+per_class_accuracy = []
 for a_class in sorted(y.unique()):
     data_temp = testing_set.loc[testing_set["y"] == a_class]
     accuracy = nn.score(data_temp.iloc[:, :-1], data_temp.iloc[:, -1])
     print("Accuracy for class {} is: {}".format(a_class, accuracy))
+    per_class_accuracy.append(accuracy)
 
-print("")
+print("\nAverage per-class accuracy: ", np.mean(per_class_accuracy))
